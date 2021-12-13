d7b2454a3d94093fc10efa352be7828f7cce6ce6
Russell Walker <russell.walker@marmalade.co.uk>
2021-12-13 16:47:51 +0000

    remove unnecessary line

--- src/Orm/ObjectMapper.php
+++ src/Orm/ObjectMapper.php
@@ -397,7 +397,6 @@ final class ObjectMapper
                         }
                     }
                 }
-                $propertyMappings[] = $propertyMapping;
             }
         }
     }
